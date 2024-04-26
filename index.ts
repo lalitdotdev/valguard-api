17f9c7b210501c630196314cceb3b3cb46be2623
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-27 04:26:14 +0530

    API(hono): Instantiate Hono app with CORS middleware for cross-origin resource sharing.

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
+import { Hono } from 'hono';
+import { Index } from '@upstash/vector';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { cors } from 'hono/cors';
+import { env } from 'hono/adapter';
 
 //  Recursive text splitter
 const semanticSplitter = new RecursiveCharacterTextSplitter({
@@ -6,3 +10,7 @@ const semanticSplitter = new RecursiveCharacterTextSplitter({
   separators: [' '],
   chunkOverlap: 8,
 });
+
+const app = new Hono();
+
+app.use(cors());
